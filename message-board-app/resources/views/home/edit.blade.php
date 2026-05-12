7fedd0484c823e594d0dfa563cc63ec9b77c1b7f
757-student004-uesugi <757-student004@la-study.com>
2026-05-12 11:03:58 +0900

    昨日段階でのLV40の共有

--- message-board-app/resources/views/home/edit.blade.php
+++ message-board-app/resources/views/home/edit.blade.php
@@ -14,4 +14,17 @@
         <button>更新する</button>
     </p>
 </form>
+<a href="#" onclick="deleteBook()">削除する</a>
+<form action="{{route('messages.destroy',$message)}}" method="post" id="delete-form">
+    @csrf
+    @method('delete')
+</form>
+<script type="text/javascript">
+    function deleteMessage() {
+        event.preventDefault();
+        if (window.confirm('本当に削除しますか？')) {
+            document.getElementById('delete-form').submit();
+        }
+    }
+</script>
 @endsection
--- message-board-app/resources/views/home/index.blade.php
+++ message-board-app/resources/views/home/index.blade.php
@@ -6,9 +6,24 @@
     <textarea name="content" rows="5" cols="20" placeholder="メッセージを入力"></textarea><br>
     <button>投稿する</button>
 </form>
-</form>
 <form action="{{ route('logout') }}" method="post">
     @csrf 
     <button>ログアウト</button>
 </form>
+
+<!-- STEP4オプション問題用 -->
+<p><a href="{{ route('profile.edit') }}">プロフィール編集</a></p>
+
+<!-- STEP5オプション問題用 -->
+<form action="{{ route('users.delete') }}" method="post">
+    @csrf 
+    @method('delete')
+    <button>退会する</button>
+</form>
+
+<hr>
+<!-- この辺にループがいる -->
+<hr>
+
+
 @endsection
